557a93a3904a4cd140bfe8f07569d3f6fad3e56f
birkin <birkin.diana@gmail.com>
2016-06-27 14:57:39 -0400

    initial commit

---
+++ app_x/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import datetime, json, logging, os, pprint
+from django.conf import settings as project_settings
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.http import HttpResponseRedirect
+
+log = logging.getLogger(__name__)
---
+++ config/settings.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+"""
+Django settings for django_template_project.
+
+Environmental variables triggered in project's env_min_djng/bin/activate, when using runserver,
+  or env_min_djng/bin/activate_this.py, when using apache via passenger.
+"""
+
+import json, logging, os
+
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['DJANGO_TEMPLATE__SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = json.loads( os.environ['DJANGO_TEMPLATE__DEBUG_JSON'] )  # will be True or False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = json.loads( os.environ['DJANGO_TEMPLATE__ADMINS_JSON'] )
+MANAGERS = ADMINS
+
+ALLOWED_HOSTS = json.loads( os.environ['DJANGO_TEMPLATE__ALLOWED_HOSTS'] )  # list
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.humanize',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'app_x',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'config.urls'
+
+WSGI_APPLICATION = 'config.passenger_wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = json.loads( os.environ['DJANGO_TEMPLATE__DATABASES_JSON'] )
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/New_York'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = False
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = os.environ['DJANGO_TEMPLATE__STATIC_URL']
+STATIC_ROOT = os.environ['DJANGO_TEMPLATE__STATIC_ROOT']  # needed for collectstatic command
+
+
+# Templates
+
+TEMPLATE_DIRS = json.loads( os.environ['DJANGO_TEMPLATE__TEMPLATE_DIRS'] )  # list
+
+
+# Email
+EMAIL_HOST = os.environ['DJANGO_TEMPLATE__EMAIL_HOST']
+EMAIL_PORT = int( os.environ['DJANGO_TEMPLATE__EMAIL_PORT'] )
+
+
+# sessions
+
+# <https://docs.djangoproject.com/en/1.9/ref/settings/#std:setting-SESSION_SAVE_EVERY_REQUEST>
+# Thinking: not that many concurrent users, and no pages where session info isn't required, so overhead is reasonable.
+SESSION_SAVE_EVERY_REQUEST = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+# logging
+
+## disable module loggers
+# existing_logger_names = logging.getLogger().manager.loggerDict.keys()
+# print '- EXISTING_LOGGER_NAMES, `%s`' % existing_logger_names
+logging.getLogger('requests').setLevel( logging.WARNING )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.FileHandler',  # note: configure server to use system's log-rotate to avoid permissions issues
+            'filename': os.environ.get(u'DJANGO_TEMPLATE__LOG_PATH'),
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'app_x': {
+            'handlers': ['logfile'],
+            'level': os.environ.get(u'DJANGO_TEMPLATE__LOG_LEVEL'),
+        },
+    }
+}
+
