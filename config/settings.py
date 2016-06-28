1845c269f256c7275e91bc0f72408842e1d80678
birkin <birkin.diana@gmail.com>
2016-06-28 08:57:04 -0400

    updates app name.

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_x',
+    'availability_app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -133,7 +133,7 @@ LOGGING = {
         },
     },
     'loggers': {
-        'app_x': {
+        'availability_app': {
             'handlers': ['logfile'],
             'level': os.environ.get(u'DJANGO_TEMPLATE__LOG_LEVEL'),
         },
