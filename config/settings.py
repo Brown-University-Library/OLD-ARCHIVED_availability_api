6bddde485bba1b4b8150bcaaa839620adf27188c
birkin <birkin.diana@gmail.com>
2016-06-28 09:00:39 -0400

    updates settings prefix.

--- config/settings.py
+++ config/settings.py
@@ -18,16 +18,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_TEMPLATE__SECRET_KEY']
+SECRET_KEY = os.environ['AVL_API__SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = json.loads( os.environ['DJANGO_TEMPLATE__DEBUG_JSON'] )  # will be True or False
+DEBUG = json.loads( os.environ['AVL_API__DEBUG_JSON'] )  # will be True or False
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = json.loads( os.environ['DJANGO_TEMPLATE__ADMINS_JSON'] )
+ADMINS = json.loads( os.environ['AVL_API__ADMINS_JSON'] )
 MANAGERS = ADMINS
 
-ALLOWED_HOSTS = json.loads( os.environ['DJANGO_TEMPLATE__ALLOWED_HOSTS'] )  # list
+ALLOWED_HOSTS = json.loads( os.environ['AVL_API__ALLOWED_HOSTS'] )  # list
 
 
 # Application definition
@@ -62,7 +62,7 @@ WSGI_APPLICATION = 'config.passenger_wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = json.loads( os.environ['DJANGO_TEMPLATE__DATABASES_JSON'] )
+DATABASES = json.loads( os.environ['AVL_API__DATABASES_JSON'] )
 
 
 # Internationalization
@@ -82,18 +82,18 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = os.environ['DJANGO_TEMPLATE__STATIC_URL']
-STATIC_ROOT = os.environ['DJANGO_TEMPLATE__STATIC_ROOT']  # needed for collectstatic command
+STATIC_URL = os.environ['AVL_API__STATIC_URL']
+STATIC_ROOT = os.environ['AVL_API__STATIC_ROOT']  # needed for collectstatic command
 
 
 # Templates
 
-TEMPLATE_DIRS = json.loads( os.environ['DJANGO_TEMPLATE__TEMPLATE_DIRS'] )  # list
+TEMPLATE_DIRS = json.loads( os.environ['AVL_API__TEMPLATE_DIRS'] )  # list
 
 
 # Email
-EMAIL_HOST = os.environ['DJANGO_TEMPLATE__EMAIL_HOST']
-EMAIL_PORT = int( os.environ['DJANGO_TEMPLATE__EMAIL_PORT'] )
+EMAIL_HOST = os.environ['AVL_API__EMAIL_HOST']
+EMAIL_PORT = int( os.environ['AVL_API__EMAIL_PORT'] )
 
 
 # sessions
@@ -123,7 +123,7 @@ LOGGING = {
         'logfile': {
             'level':'DEBUG',
             'class':'logging.FileHandler',  # note: configure server to use system's log-rotate to avoid permissions issues
-            'filename': os.environ.get(u'DJANGO_TEMPLATE__LOG_PATH'),
+            'filename': os.environ.get(u'AVL_API__LOG_PATH'),
             'formatter': 'standard',
         },
         'console':{
@@ -135,7 +135,7 @@ LOGGING = {
     'loggers': {
         'availability_app': {
             'handlers': ['logfile'],
-            'level': os.environ.get(u'DJANGO_TEMPLATE__LOG_LEVEL'),
+            'level': os.environ.get(u'AVL_API__LOG_LEVEL'),
         },
     }
 }
