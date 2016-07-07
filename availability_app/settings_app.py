bb8d4d9fd3e01356121a10769bb86c50c78a5b8b
birkin <birkin.diana@gmail.com>
2016-07-07 15:13:29 -0400

    adds setting for additional found isbn.

--- availability_app/settings_app.py
+++ availability_app/settings_app.py
@@ -9,3 +9,8 @@ import os
 Z_HOST = unicode( os.environ['AVL_API__Z_HOST'] )
 Z_PORT = unicode( os.environ['AVL_API__Z_PORT'] )
 Z_TYPE = unicode( os.environ['AVL_API__Z_TYPE'] )
+
+
+## for tests
+TEST_ISBN_FOUND_01 = unicode( os.environ['AVL_API__ISBN_FOUND_01'] )
+TEST_ISBN_FOUND_02 = unicode( os.environ['AVL_API__ISBN_FOUND_02'] )
