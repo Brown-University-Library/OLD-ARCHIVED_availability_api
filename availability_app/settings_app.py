869b4fa73472bfb1497d6309f6ed90b4e31ad8a8
birkin <birkin.diana@gmail.com>
2016-07-07 12:31:55 -0400

    adds required z39.50 settings.

--- availability_app/settings_app.py
+++ availability_app/settings_app.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
 import os
 
 
-FOO = 'BAR'
-
-FOO2 = os.environ['FOO2']
+## for z39.50 lookups
+Z_HOST = unicode( os.environ['AVL_API__Z_HOST'] )
+Z_PORT = unicode( os.environ['AVL_API__Z_PORT'] )
+Z_TYPE = unicode( os.environ['AVL_API__Z_TYPE'] )
