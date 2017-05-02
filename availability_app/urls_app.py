e221c00be33a4074a8bdf806eca6e5468e9f9834
birkin <birkin.diana@gmail.com>
2017-05-02 10:27:31 -0400

    disables app-level urls.py

--- availability_app/urls_app.py
+++ availability_app/urls_app.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-from django.conf.urls import patterns, include, url
-from django.views.generic import RedirectView
+# from __future__ import unicode_literals
+# from django.conf.urls import patterns, include, url
+# from django.views.generic import RedirectView
 
 
-urlpatterns = patterns('',
+# urlpatterns = patterns('',
 
-    url( r'^info/$',  'availability_app.views.hi', name='info_url' ),
+#     url( r'^info/$',  'availability_app.views.hi', name='info_url' ),
 
-    url( r'^v2/(?P<id_type>.*)/(?P<id_value>.*)/$',  'availability_app.views.handler', name='handler_url' ),  # id_type, 'isbn' or 'oclc'
+#     url( r'^v2/(?P<id_type>.*)/(?P<id_value>.*)/$',  'availability_app.views.handler', name='handler_url' ),  # id_type, 'isbn' or 'oclc'
 
-    url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
+#     url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
 
-    )
+#     )
