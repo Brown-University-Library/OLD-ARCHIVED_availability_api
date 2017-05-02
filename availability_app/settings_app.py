9a00d3e326775f9a9e82e1c5d713036b0af6fe38
birkin <birkin.diana@gmail.com>
2017-05-02 10:27:07 -0400

    removes unneeded unicode specification.

--- availability_app/settings_app.py
+++ availability_app/settings_app.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-
 import os
 
 
 ## for z39.50 lookups
-Z_HOST = unicode( os.environ['AVL_API__Z_HOST'] )
-Z_PORT = unicode( os.environ['AVL_API__Z_PORT'] )
-Z_TYPE = unicode( os.environ['AVL_API__Z_TYPE'] )
+Z_HOST = os.environ['AVL_API__Z_HOST']
+Z_PORT = os.environ['AVL_API__Z_PORT']
+Z_TYPE = os.environ['AVL_API__Z_TYPE']
 
 
 ## for tests
-TEST_ISBN_FOUND_01 = unicode( os.environ['AVL_API__ISBN_FOUND_01'] )
-TEST_ISBN_FOUND_02 = unicode( os.environ['AVL_API__ISBN_FOUND_02'] )
+TEST_ISBN_FOUND_01 = os.environ['AVL_API__ISBN_FOUND_01']
+TEST_ISBN_FOUND_02 = os.environ['AVL_API__ISBN_FOUND_02']
