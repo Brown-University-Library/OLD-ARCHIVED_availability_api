ec71bce12495c3fafc6a518374037cdf3cdd58ec
birkin <birkin.diana@gmail.com>
2018-02-07 16:51:55 -0500

    updates urls.

--- config/urls.py
+++ config/urls.py
@@ -10,13 +10,13 @@ admin.autodiscover()
 
 urlpatterns = [
 
-    url( r'^admin/', include(admin.site.urls) ),  # eg host/project_x/admin/
+    url( r'^admin/', include(admin.site.urls) ),
 
-    # url( r'^', include('availability_app.urls_app') ),  # eg host/project_x/anything/
+    url( r'^info/$',  avl_views.info, name='info_url' ),
 
-    url( r'^info/$',  avl_views.hi, name='info_url' ),
+    url( r'^v1/(?P<id_type>.*)/(?P<id_value>.*)/$',  avl_views.ezb_v1, name='ezb_v1_url' ),
 
-    url( r'^v2/(?P<id_type>.*)/(?P<id_value>.*)/$',  avl_views.handler, name='handler_url' ),  # id_type, 'isbn' or 'oclc'
+    url( r'^v2/(?P<id_type>.*)/(?P<id_value>.*)/$',  avl_views.ezb_v2, name='ezb_v2_url' ),
 
     url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
 
