5b38659594edc3bc4915dab407b3983c0bc23d6e
birkin <birkin.diana@gmail.com>
2018-02-07 16:52:33 -0500

    updates view stubs.

--- availability_app/views.py
+++ availability_app/views.py
@@ -2,7 +2,7 @@
 
 import datetime, json, logging, os, pprint
 
-from availability_app.utils.app_helper import HandlerHelper
+from availability_app.lib.app_helper import HandlerHelper
 from availability_app import settings_app
 from django.conf import settings as project_settings
 from django.contrib.auth import logout
@@ -14,14 +14,15 @@ log = logging.getLogger(__name__)
 helper = HandlerHelper( settings_app.Z_HOST, settings_app.Z_PORT, settings_app.Z_TYPE )
 
 
-def hi( request ):
+def info( request ):
     """ Returns simplest response. """
-    log.debug( 'starting hi()' )
+    log.debug( 'starting info()' )
     now = datetime.datetime.now()
-    return HttpResponse( '<p>hi</p> <p>( {} )</p>'.format(unicode(now)) )
+    return HttpResponse( '<p>time: %s</p>' % str(now) )
 
 
-def handler( request, id_type, id_value ):
+def ezb_v1( request, id_type, id_value ):
+    """ Handles existing easyborrow-api call. """
     params = request.GET
     log.debug( 'starting; id_type, `{}`; id_value, `{}`; get, `{}`'.format(id_type, id_value, pprint.pformat(params)) )
     # helper = app_helper.HandlerHelper()
@@ -35,3 +36,8 @@ def handler( request, id_type, id_value ):
         response = helper.build_response_dict( id_type, id_value, request.GET.get('show_marc', '') )
         jsn = json.dumps( {u'query': query, u'response': response}, sort_keys=True, indent=2 )
     return HttpResponse( jsn, content_type='application/javascript; charset=utf-8' )
+
+
+def ezb_v2( request, id_type, id_value ):
+    """ Handles upcoming easyborrow-api call. """
+    return HttpResponse( 'ezb_v2_url handling coming' )
