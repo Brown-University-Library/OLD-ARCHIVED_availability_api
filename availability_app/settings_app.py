2dfaec3a0215ace9e218a0d55984dc5cf6f15992
birkin <birkin.diana@gmail.com>
2018-08-27 10:11:04 -0400

    updates settings.

--- availability_app/settings_app.py
+++ availability_app/settings_app.py
@@ -11,6 +11,10 @@ Z_HOST = os.environ['AVL_API__Z_HOST']
 Z_PORT = os.environ['AVL_API__Z_PORT']
 Z_TYPE = os.environ['AVL_API__Z_TYPE']
 
+CMD_START_DIR_PATH = os.environ['AVL_API__CMD_START_DIR_PATH']
+CMD_ENV_BIN_DIR_PATH = os.environ['AVL_API__CMD_ENV_BIN_DIR_PATH']
+CMD_WRAPPER_DIR_PATH = os.environ['AVL_API__CMD_WRAPPER_DIR_PATH']
+
 
 ## for tests
 TEST_ISBN_FOUND_01 = os.environ['AVL_API__ISBN_FOUND_01']
