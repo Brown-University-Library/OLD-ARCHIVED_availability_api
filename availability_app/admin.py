ff2f5f4a4ecbb192803bd237eb1a8c5376746dd4
birkin <birkin.diana@gmail.com>
2018-09-05 13:44:01 -0400

    adds model and its admin.

--- availability_app/admin.py
+++ availability_app/admin.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
+from .models import Tracker
 from django.contrib import admin
+
+
+class TrackerAdmin( admin.ModelAdmin ):
+    list_display = [ 'count_date', 'ezb_isbn_count', 'ezb_bib_count', 'unofficial_access_count', 'dt_created', 'dt_modified' ]
+    ordering = [ '-count_date' ]
+    readonly_fields = [ 'count_date', 'ezb_isbn_count', 'ezb_bib_count', 'unofficial_access_count', 'dt_created', 'dt_modified' ]
+
+
+admin.site.register( Tracker, TrackerAdmin )
