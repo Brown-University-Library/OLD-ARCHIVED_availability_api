4b4e4ba6e1dc7a4f954e98c3be02aa1d1b887019
birkin <birkin.diana@gmail.com>
2018-09-06 10:05:56 -0400

    stubs analyzer code.

--- availability_app/lib/log_checker.py
+++ availability_app/lib/log_checker.py
@@ -5,9 +5,11 @@ For log analysis.
 """
 
 import datetime, json, logging, os, pprint
+from log_parser import LogParser
 
 
 log = logging.getLogger(__name__)
+parser = LogParser()
 
 
 class LogAnalyzer( object ):
@@ -19,7 +21,19 @@ class LogAnalyzer( object ):
     def check_logs( self ):
         """ Checks recent logs.
             Called by: ```if __name__ == '__main__'```; triggered via cronjob """
+        last_check_date = self.get_last_check_date()
+        worthy_entries = self.check_files( last_check_date )
+        for entry in worthy_entries:
+            log_parts_dct = parser.make_parts( entry )
+            entry_date = parser.get_date( log_parts_dct['date_string'] )
+            data_dct = json.loads( log_parts_dct['json_string'] )
+            self.process_entry( entry_date, data_dct )
         return
+
+    def process_entry( self, entry_date, data_dct ):
+        """ Updates db with data.
+            Called by check_logs() """
+        pass
 
     ## end LogAnalyzer()
 
