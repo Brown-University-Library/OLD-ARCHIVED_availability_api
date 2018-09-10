4699560330aa60876fea722dff3391e73f1b9991
birkin <birkin.diana@gmail.com>
2018-09-10 10:55:25 -0400

    builds out log-file inspection.

--- availability_app/lib/log_checker.py
+++ availability_app/lib/log_checker.py
@@ -4,7 +4,7 @@
 For log analysis.
 """
 
-import datetime, json, logging, os, pprint, sys
+import datetime, glob, json, logging, os, pprint, sys
 
 ## configure django environment
 import django
@@ -35,21 +35,22 @@ class LogAnalyzer( object ):
 
     def __init__( self ):
         self.last_checked_path = os.environ['AVL_API__LAST_CHECKED_PATH']
+        self.log_dir_path = os.path.dirname( os.environ['AVL_API__LOG_PATH'] )
 
     def check_logs( self ):
         """ Checks recent logs.
             Called by: ```if __name__ == '__main__'```; triggered via cronjob """
         last_check_date = self.get_last_check_date()
-        worthy_entries = self.check_files( last_check_date )
+        worthy_entries = self.scan_log_files( last_check_date )
         for entry in worthy_entries:
-            log_parts_dct = parser.make_parts( entry )
+            # log_parts_dct = parser.make_parts( entry )
             entry_datetime = parser.get_date( log_parts_dct['date_string'] )
             data_dct = json.loads( log_parts_dct['json_string'] )
             self.process_entry( entry_datetime, data_dct )
         return
 
     def get_last_check_date( self ):
-        """ Returns date of last check.
+        """ Returns date of last-log-entry checked.
             Called by check_logs() """
         with open( self.last_checked_path ) as fh:
             dt_str_1 = json.loads( fh.read() )
@@ -57,6 +58,27 @@ class LogAnalyzer( object ):
             dt= datetime.datetime.strptime( dt_str_2, '%Y-%m-%dT%H:%M:%S.%f' )
             log.debug( 'dt, `%s`' % dt )
             return dt
+
+    def scan_log_files( self, last_check_date ):
+        """ Returns list of log-entry dcts to process (since last_check_date).
+            Called by check_logs() """
+        worthy_data_dct_entries = []
+        # log_file_list = sorted( glob.glob('%s/availability_api_stats*' % self.log_dir_path) )
+        # log_file_list = sorted( glob.glob('%s/*.log' % self.log_dir_path) )
+        glob_path_string = '%s/availability_api_stats*' % self.log_dir_path
+        log.debug( 'glob_path_string, ```%s```' % glob_path_string )
+        log_file_list = sorted( glob.glob(glob_path_string) )
+        log.debug( 'log_file_list, ```%s```' % log_file_list )
+        for file_path in log_file_list:
+            with open( file_path ) as fh:
+                for line in fh.readlines():
+                    log_line_parts_dct = parser.make_parts( line )
+                    log.debug( 'log_line_parts_dct, ```%s```' % log_line_parts_dct )
+                    entry_datetime = parser.get_date( log_line_parts_dct['date_string'] )
+                    log.debug( 'entry_datetime, ```%s```' % entry_datetime )
+        log.debug( 'worthy_data_dct_entries, ```%s```' % worthy_data_dct_entries )
+        return worthy_data_dct_entries
+
 
     def process_entry( self, entry_datetime, data_dct ):
         """ Updates db with data.
