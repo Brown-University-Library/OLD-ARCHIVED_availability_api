5d073184bd29bf2dfcc4885ed816ef40faf81cc7
birkin <birkin.diana@gmail.com>
2018-10-03 11:21:33 -0400

    implements isbn validity.

--- availability_app/lib/log_parser.py
+++ availability_app/lib/log_parser.py
@@ -5,6 +5,7 @@ Parser for custom log.
 """
 
 import datetime, json, logging, os, pickle, pprint, subprocess, urllib
+import isbnlib
 from availability_app import settings_app
 
 
@@ -17,37 +18,22 @@ class LogParser( object ):
     def __init__( self ):
         pass
 
-    # def make_parts( self, line ):
-    #     """ Splits line into date-part and json-data-part.
-    #         Called by: TBD """
-    #     parts_dct = { 'date_string': '', 'json_string': '' }
-    #     parts = line.split()
-    #     parts_dct['date_string'] = parts[0] + ' ' + parts[1]
-    #     j_parts = parts[4:]
-    #     parts_dct['json_string'] = ' '.join( j_parts )
-    #     log.debug( 'parts_dct, ```%s```' % pprint.pformat(parts_dct) )
-    #     return parts_dct
-
-    # def get_date( self, date_string ):
-    #     """ Returns date-object.
-    #         Called by: TBD """
-    #     dt = datetime.datetime.strptime( date_string, '[%d/%b/%Y %H:%M:%S]' )
-    #     return dt
-
     def get_date( self, date_string ):
         """ Returns date-object.
-            Called by: TBD """
+            Called by: LogAnalyzer.process_entry() and LogAnalyzer.update_trckr() """
         dt_str_1 = date_string[0:26]  # in case 'Z' or '+' time-zone stuff is appended
         dt = datetime.datetime.strptime( dt_str_1, '%Y-%m-%dT%H:%M:%S.%f' )
         log.debug( 'dt, `%s`' % str(dt) )
         return dt
 
-
-    # def get_json( self, json_string ):
-    #     """ Returns date-object.
-    #         Function only called by test, to validate the json. """
-    #     dct = json.loads( json_string )
-    #     return dct
-
+    def validate_isbn( self, isbn ):
+        """ Returns boolean.
+            Called by TBD """
+        if isbnlib.is_isbn10(isbn) or isbnlib.is_isbn13(isbn):
+            rslt = True
+        else:
+            rslt = False
+            log.debug( 'isbn, `%s` is not valid' )
+        return rslt
 
     ## end LogParser()
