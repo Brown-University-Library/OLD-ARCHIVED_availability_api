547710ab77d6c0aa106c16dd1fb06cd62b7095a9
birkin <birkin.diana@gmail.com>
2019-08-27 10:41:35 -0400

    builds out response.

--- availability_app/lib/bib_v2.py
+++ availability_app/lib/bib_v2.py
@@ -32,31 +32,57 @@ class BibInfo:
         """ Grabs and processes data from Sierra.
             Called by: views.v2_bib() """
         sierra = SierraConnector()  # instantiated here to get fresh token
-        sierra.get_bibinfo( bib )
-        return 'foo'
+        raw_data = sierra.get_bibinfo( bib )
+        items = self.summarize_data( raw_data )
+        response_dct = { 'items': items, 'items_count': len(items), 'sierra_api': raw_data, 'sierra_api_query': sierra.url }
+        log.debug( f'response_dct, ```{response_dct}```' )
+        return response_dct
 
-    def build_response_dct( self, data_dct, start_stamp ):
-        """ Yup.
-            Called by views.v2_bib() """
-        return { 'time_taken': str( datetime.datetime.now() - start_stamp ) }
+    def summarize_data( self, raw_data ):
+        """ Extracts essential data from sierra-api data.
+            Called by prep_data() """
+        items = []
+        for entry in raw_data['entries']:
+            item_dct = {
+                'barcode': entry['barcode'],
+                'callnumber': self.build_callnumber( entry ),
+                'item_id': self.build_item_id( entry['id'] ),
+                'location': entry['location']['name'],
+                'status': entry['status']['display']
+            }
+            items.append( item_dct )
+        log.debug( f'items, ```{pprint.pformat(items)}```' )
+        return items
 
-    # def build_stats_dct( self, query_url, referrer, user_agent, ip ):
-    #     """ Builds and logs data for stats.
-    #         Called by build_query_dct() """
-    #     stats_dct = { 'datetime': datetime.datetime.now().isoformat(), 'query': query_url, 'referrer': None, 'user_agent': user_agent, 'ip': ip }
-    #     if referrer:
-    #         output = urllib.parse.urlparse( referrer )
-    #         stats_dct['referrer'] = output
-    #     slog.info( json.dumps(stats_dct) )
-    #     return
+    def build_callnumber( self, entry ):
+        """ Adds data to default callnumber field.
+            Called by summarize_data() """
+        initial_callnumber = entry['callNumber']
+        addition = ''
+        if 'varFields' in entry.keys():
+            for var_field_dct in entry['varFields']:
+                if var_field_dct.get( 'fieldTag', '' ) == 'v':
+                    addition = var_field_dct['content']  # i.e. "Box 10"
+        built_callnumber = f'{initial_callnumber} {addition}'.strip()
+        log.debug( f'built_callnumber, `{built_callnumber}`' )
+        return built_callnumber
 
-    def add_check_digit( self, raw_item_id ):
-        """ Not yet used.
-            Logic based on: <https://csdirect.iii.com/sierrahelp/Content/sril/sril_records_numbers.html> """
+    def build_item_id( self, initial_item_id ):
+        """ Adds '.i' and check-digit to item-id.
+            Called by summarize_data() """
+        check_digit = self.build_check_digit( initial_item_id )
+        built_item_id = f'.i{initial_item_id}{check_digit}'
+        log.debug( 'built_item_id, `{built_item_id}`' )
+        return built_item_id
+
+    def build_check_digit( self, raw_item_id ):
+        """ Calculates check-digit.
+            Logic based on: <https://csdirect.iii.com/sierrahelp/Content/sril/sril_records_numbers.html>
+            Called by build_item_id() """
         data = str(raw_item_id).replace('-', '').replace(' ', '')  # Removes Hyphens and Spaces
-        reversed = ''.join( reversed(data) )
+        reversed_data = ''.join( reversed(data) )
         total = 0
-        for counter,character in enumerate( reversed ):
+        for counter,character in enumerate( reversed_data ):
             log.debug( f'character, `{character}`' )
             one_index = counter + 1
             log.debug( f'one_index, `{one_index}`' )
@@ -71,6 +97,28 @@ class BibInfo:
             check_digit = 'x'
         log.debug( f'check_digit, `{check_digit}`' )
         return check_digit
+
+    def build_response_dct( self, data_dct, start_stamp ):
+        """ Yup.
+            Called by views.v2_bib() """
+        response_dct = data_dct
+        # response_dct = {
+        #     'time_taken': str( datetime.datetime.now() - start_stamp ),
+        #     'sierra_api': data_dct
+        #      }
+        response_dct['time_taken'] = str( datetime.datetime.now() - start_stamp )
+        log.debug( f'response_dct, ```{pprint.pformat(response_dct)}```' )
+        return response_dct
+
+    # def build_stats_dct( self, query_url, referrer, user_agent, ip ):
+    #     """ Builds and logs data for stats.
+    #         Called by build_query_dct() """
+    #     stats_dct = { 'datetime': datetime.datetime.now().isoformat(), 'query': query_url, 'referrer': None, 'user_agent': user_agent, 'ip': ip }
+    #     if referrer:
+    #         output = urllib.parse.urlparse( referrer )
+    #         stats_dct['referrer'] = output
+    #     slog.info( json.dumps(stats_dct) )
+    #     return
 
     ## end class BibInfo
 
