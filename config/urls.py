75583edbd86724a4774ab4221846426e2b3f0fcb
birkin <birkin.diana@gmail.com>
2019-10-26 10:07:38 -0400

    stubs async url structure.

--- config/urls.py
+++ config/urls.py
@@ -11,14 +11,15 @@ admin.autodiscover()
 urlpatterns = [
 
     ## primary app urls...
-
     url( r'^v1/(?P<id_type>.*)/(?P<id_value>.*)/$', avl_views.ezb_v1, name='ezb_v1_url' ),
     url( r'^v1_stats/$', avl_views.ezb_v1_stats, name='ezb_v1_stats_url' ),
-    # url( r'^v2/(?P<id_type>.*)/(?P<id_value>.*)/$', avl_views.ezb_v2, name='ezb_v2_url' ),
     url( r'^v2/bib_items/(?P<bib_value>.*)/$', avl_views.v2_bib_items, name='v2_bib_items_url' ),
     url( r'^locations_and_statuses/$', avl_views.locations_and_statuses, name='locations_and_statuses_url' ),
-    url( r'^async/$', avl_views.concurrency_test, name='async_url' ),
     url( r'^admin/', include(admin.site.urls) ),
+
+    ## demo urls...
+    url( r'^async/$', avl_views.concurrency_test, name='async_url' ),
+    url( r'^v2/bib_items_async/(?P<bib_value>.*)/$', avl_views.v2_bib_items_async, name='v2_bib_items_async_url' ),
 
     ## support urls...
     url( r'^info/$',  avl_views.version, name='info_url' ),  # historical url
